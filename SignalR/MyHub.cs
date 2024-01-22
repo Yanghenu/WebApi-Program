652ce8b6bba7570753d7480fbfd00dfcd0556fbf
Yanghenu <67721696+Yanghenu@users.noreply.github.com>
2024-01-22 18:06:12 +0800

    Update MyHub.cs

--- SignalR/MyHub.cs
+++ SignalR/MyHub.cs
@@ -4,9 +4,31 @@ namespace SignalR
 {
     public class MyHub:Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"建立新的SignalR连接:{Context.ConnectionId}");
+            RecordUser();
+            await base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Console.WriteLine($"关闭SignalR连接:{Context.ConnectionId}|Exception:{exception?.Message}");
+            RecordUser();
+            return base.OnDisconnectedAsync(exception);
+        }
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        /// <summary>
+        /// 记录
+        /// </summary>
+        private void RecordUser()
+        {
+            Console.WriteLine($"用户的ConnectionId：" + Context.ConnectionId + ";Name:" + Context.User.Identity.Name);
         }
     }
 }
